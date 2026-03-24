ed595a4d624a9525d1716e8550a3e3568a336f4f
likhithsait <likhithsai.t@narayanagroup.com>
2026-03-24 13:08:17 +0530

    1

--- student-admin/src/components/admin/ConversationList.tsx
+++ student-admin/src/components/admin/ConversationList.tsx
@@ -22,7 +22,7 @@ export const ConversationList = ({
 }: ConversationListProps) => {
   return (
     <div className="h-full flex flex-col">
-      <div className="relative mb-4">
+      <div className="relative mb-0">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
         <input
           type="text"
